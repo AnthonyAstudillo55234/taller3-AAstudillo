3455b2f19a54e702595bb8c9b3b600787afe20fa
Anthony <anthony.astudillo@epn.edu.ec>
2024-10-16 18:24:46 -0500

    first commit

---
+++ arreglos.js
@@ -0,0 +1,22 @@
+// ARREGLOS ANTHONY ASTUDILLO
+const products = [
+    {
+        name:"Sceptre Monitor curvo para juegos",
+        price:139.05,
+        category:"Gaming"
+    },
+    {
+        name:"MAONO Micrófono USB para juegos",
+        price:38.24,
+        category:"Gaming"
+    },
+    {
+        name:"Mini refrigerador de bebidas con luz para juegos",
+        price:40,
+        category:"Gaming"
+    },
+]
+
+products.forEach(product => {
+    console.log(product.name);
+});
---
+++ objetos.js
@@ -0,0 +1,14 @@
+// OBJETOS ANTHONY ASTUDILLO
+const casePc = {
+    name:"Skytech Gaming Nebula Gaming PC de sobremesa",
+    price:850,
+    colors:
+    {
+        colorB:"blue",
+        colorG:"green",
+        colorR:"red"
+    },
+    brand:"Skytech Gaming"
+}
+
+console.log(casePc.name,casePc.colors);
